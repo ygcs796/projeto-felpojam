dc5e6cd1c82ab88b007cb6637ba0df78ac7a2dd2
ygcs796 <yurigabryel3@gmail.com>
2026-02-19 00:02:23 -0300

    Tela de vitória e derrota placeholder
    Fiz uma tela de vitória e derrota básica pra gente pelo menos ter alguma coisa. Todos os botões estão funcionando perfeitamente

---
+++ derrota.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_restart_button_down() -> void:
+	# aqui eu vou colocar a próxima cena
+	#get_tree().change_scene_to_file()
+	print("Recomeçando")
+	pass
+
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
