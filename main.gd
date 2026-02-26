11958401f8c87994b4d921c567742dc2e65e55c2
ygcs796 <yurigabryel3@gmail.com>
2026-02-25 23:57:43 -0300

    Pequenos ajustes
    Coloquei o inimigo para ir em direção à bola

--- main.gd
+++ main.gd
@@ -2,12 +2,25 @@ extends Node2D
 
 @onready var player = $Player
 @onready var adversario = $adversario
+@onready var bola = $bola_volei
+
+#@onready var posicao_inicial_bola_x = bola.position.x
+#@onready var posicao_inicial_bola_y = bola.position.y
+
 
 func _ready() -> void:
 	pass
 	
 func _process(delta: float) -> void:
-	var diferenca_pos = (player.position.x - adversario.position.x)
-	var velocidade = diferenca_pos / (delta * 100)
+	var diferenca_pos = (bola.position.x - adversario.position.x)
+	var velocidade = diferenca_pos / (delta * 500)
 	adversario.position.x += velocidade * delta
 	pass
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_R:
+			print("R apertado")
+			bola.position.x = 40
+			bola.position.y = 56
+			print("processo feito")
