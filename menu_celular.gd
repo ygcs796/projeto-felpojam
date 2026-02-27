5496f49f9c83ac47c18962cbe3aa06f253d4c27e
ygcs796 <yurigabryel3@gmail.com>
2026-02-27 14:36:09 -0300

    Mudanças
    Fiz uma tela de controles, mudei um pouco os botões do menu, conectei os sinais dos botões e agora temos 3 telas: start, créditos e controles. Ainda faltam algumas coisas

--- menu_celular.gd
+++ menu_celular.gd
@@ -4,6 +4,16 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://iglu_amanhecendo.tscn")
 
+func _on_settings_pressed() -> void:
+	#get_tree().change_scene_to_file("res://creditos.tscn")
+	pass # Replace with function body.
 
-func _on_credit_pressed() -> void:
+
+func _on_controles_pressed() -> void:
+	get_tree().change_scene_to_file("res://controles.tscn")
+	pass # Replace with function body.
+
+
+func _on_creditos_pressed() -> void:
 	get_tree().change_scene_to_file("res://creditos.tscn")
+	pass # Replace with function body.
