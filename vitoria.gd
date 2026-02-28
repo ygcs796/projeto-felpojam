612c8319c1847a0142f91eeec48fc67383018e4f
ygcs796 <yurigabryel3@gmail.com>
2026-02-27 21:51:53 -0300

    Telas de derrota e vitória
    Já coloquei aqui

---
+++ vitoria.gd
@@ -0,0 +1,7 @@
+extends Control	
+
+func _on_continue_button_down() -> void:
+	# COLOCAR PRÓXIMA CENA AQUI
+	#get_tree().change_scene_to_file()
+	print("Continuando")
+	pass # Replace with function body.
