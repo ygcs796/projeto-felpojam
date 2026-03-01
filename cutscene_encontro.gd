479cbe8cf64a622228d3f073fe972859db652275
Maria Teresa <mteresamatosochaves@gmail.com>
2026-03-01 00:29:17 -0300

    Santa Catarina
    Transições de Santa Catarina adicionadas

--- cutscene_encontro.gd
+++ cutscene_encontro.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
-@onready var anim = $transicao_circulo/AnimationPlayer
+@onready var anim: AnimationPlayer = $transicao_circulo/AnimationPlayer
+var transitioning := false
 
-func _ready():
+func _ready() -> void:
 	anim.play("aparecer")
 
 func _input(event: InputEvent) -> void:
-	
-	if Input.is_key_pressed(KEY_ENTER):
-		
+	if transitioning:
+		return
+
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		transitioning = true
+
 		anim.play("apagar")
 		await anim.animation_finished
+
 		get_tree().change_scene_to_file("res://cenario_santa_catarina.tscn")
